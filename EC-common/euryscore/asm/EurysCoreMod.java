43ec3186a6925087833a01b52a94089525ca848a
Eurymachus <reflex_ion@hotmail.com>
2012-12-12 20:24:27 +0000

    Added EurysCoreMod component.

---
+++ EC-common/euryscore/asm/EurysCoreMod.java
@@ -0,0 +1,40 @@
+package euryscore.asm;
+
+import java.util.Map;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;;
+
+@TransformerExclusions(("eurysmods.asm"))
+public class EurysCoreMod implements IFMLLoadingPlugin {
+
+	@Override
+	public String[] getLibraryRequestClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getASMTransformerClass() {
+		return new String[] {"euryscore.asm.EurysAccessTransformer"};
+	}
+
+	@Override
+	public String getModContainerClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSetupClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
