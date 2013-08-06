d17355885e63c9e639996f1370317decfe1158be
Eurymachus <reflex_ion@hotmail.com>
2013-08-06 22:47:19 +0100

    Fixed oops moment with proxy

--- SV-common/slimevoidlib/core/lib/CoreLib.java
+++ SV-common/slimevoidlib/core/lib/CoreLib.java
@@ -4,6 +4,6 @@ public class CoreLib {
 	public static final String MOD_ID = "SlimevoidLib";
 	public static final String MOD_NAME = "Slimevoid Library";
 	public static final String MOD_VERSION = "2.0.2.0";
-	public static final String MOD_CLIENT_PROXY = "slimevoid.lib.proxy.SV_ClientProxy";
-	public static final String MOD_COMMON_PROXY = "slimevoid.lib.proxy.SV_CommonProxy";
+	public static final String MOD_CLIENT_PROXY = "slimevoidlib.proxy.SV_ClientProxy";
+	public static final String MOD_COMMON_PROXY = "slimevoidlib.proxy.SV_CommonProxy";
 }
