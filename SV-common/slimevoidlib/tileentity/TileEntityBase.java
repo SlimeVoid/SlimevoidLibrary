e072d2f817b4e302d70772bbeab1cd26ed9d6eea
Eurymachus <reflex_ion@hotmail.com>
2013-09-06 15:57:38 +0100

    Cleanup

--- SV-common/slimevoidlib/tileentity/TileEntityBase.java
+++ SV-common/slimevoidlib/tileentity/TileEntityBase.java
@@ -20,8 +20,6 @@ import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Icon;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
 import slimevoidlib.blocks.BlockBase;
 import slimevoidlib.core.lib.NBTLib;
