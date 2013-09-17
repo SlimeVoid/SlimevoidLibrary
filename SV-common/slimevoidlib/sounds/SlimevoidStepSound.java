553be884e07f59772f26a26a7d0554d90cf9cf7a
Eurymachus <reflex_ion@hotmail.com>
2013-09-17 20:02:34 +0100

    Added SlimevoidStepSound implementation

---
+++ SV-common/slimevoidlib/sounds/SlimevoidStepSound.java
@@ -0,0 +1,33 @@
+package slimevoidlib.sounds;
+
+import net.minecraft.block.StepSound;
+
+public class SlimevoidStepSound extends StepSound {
+
+	/**
+	 * This is a facade class used for the BlockBase of Slimevoid Mods
+	 * 
+	 * @param stepSoundName
+	 * @param volume
+	 * @param pitch
+	 */
+	public SlimevoidStepSound(String stepSoundName, float volume, float pitch) {
+		super(stepSoundName, volume, pitch);
+	}
+
+	@Override
+	public String getBreakSound() {
+		return "dig.slimevoid." + this.stepSoundName;
+	}
+
+	@Override
+	public String getStepSound() {
+		return "step.slimevoid." + this.stepSoundName;
+	}
+
+	@Override
+	public String getPlaceSound() {
+		return "place.slimevoid." + this.stepSoundName;
+	}
+
+}
