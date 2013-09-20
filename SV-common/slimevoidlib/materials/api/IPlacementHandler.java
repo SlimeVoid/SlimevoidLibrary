1b3bea82a58e59219405ea97c027846f4cb3d17d
Allen Cook <alcook16@gmail.com>
2013-09-20 19:00:27 -0400

    Rename Placement Functions

--- SV-common/slimevoidlib/materials/api/IPlacementHandler.java
+++ SV-common/slimevoidlib/materials/api/IPlacementHandler.java
@@ -9,16 +9,14 @@ import net.minecraft.world.World;
 
 public interface IPlacementHandler {
 
-	public abstract boolean onPlaceSlope(ItemStack itemstack, EntityPlayer entityplayer, World world, int x, int y, int z, int i);
-
-	public abstract String getSlopeName(int i, int j);
+	public abstract boolean onPlaceBlock(ItemStack itemstack, EntityPlayer entityplayer, World world, int x, int y, int z, int i);
 
 	public abstract void addCreativeItems(int i, CreativeTabs tabs, List tabslist, int matIndex);
 
-	public abstract boolean placeSlopeAt(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ, int metadata);
+	public abstract boolean placeBlockAt(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ, int metadata);
 
-	public abstract String getName();
+	public abstract String getUnlocalizedName();
 
-	public abstract String getTabDisplayName();
+	public abstract String getLocalizedName();
 
 }
