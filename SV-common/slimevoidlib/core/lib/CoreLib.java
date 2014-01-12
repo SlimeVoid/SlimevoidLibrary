485d4ef61210568d55862a7c20ff4f70452fa261
Eurymachus <reflex_ion@hotmail.com>
2014-01-12 16:40:08 +0000

    Version 2.0.3.4

--- SV-common/slimevoidlib/core/lib/CoreLib.java
+++ SV-common/slimevoidlib/core/lib/CoreLib.java
@@ -3,7 +3,7 @@ package slimevoidlib.core.lib;
 public class CoreLib {
 	public static final String	MOD_ID				= "SlimevoidLib";
 	public static final String	MOD_NAME			= "Slimevoid Library";
-	public static final String	MOD_VERSION			= "2.0.3.3";
+	public static final String	MOD_VERSION			= "2.0.3.4";
 	public static final String	MOD_CLIENT_PROXY	= "slimevoidlib.proxy.SV_ClientProxy";
 	public static final String	MOD_COMMON_PROXY	= "slimevoidlib.proxy.SV_CommonProxy";
 }
