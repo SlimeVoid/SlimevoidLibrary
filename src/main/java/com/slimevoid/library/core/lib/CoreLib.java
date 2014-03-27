47f4227963e7ecd66caf0dcfbbf2b4cee433b33a
Eurymachus <reflex_ion@hotmail.com>
2014-03-27 19:00:15 +0000

    Build script

--- src/main/java/com/slimevoid/library/core/lib/CoreLib.java
+++ src/main/java/com/slimevoid/library/core/lib/CoreLib.java
@@ -3,7 +3,7 @@ package com.slimevoid.library.core.lib;
 public class CoreLib {
     public static final String MOD_ID           = "SlimevoidLib";
     public static final String MOD_NAME         = "Slimevoid Library";
-    public static final String MOD_VERSION      = "2.0.4.1";
+    public static final String MOD_VERSION      = "2.0.4.5";
     public static final String MOD_CLIENT_PROXY = "com.slimevoid.library.proxy.SV_ClientProxy";
     public static final String MOD_COMMON_PROXY = "com.slimevoid.library.proxy.SV_CommonProxy";
 }
