a398b2d91b89993b2a0b906cbfeb4b1d1feada05
Eurymachus <reflex_ion@hotmail.com>
2014-03-28 11:12:40 +0000

    Forgot to refactor proxy

--- src/main/java/net/slimevoid/library/core/lib/CoreLib.java
+++ src/main/java/net/slimevoid/library/core/lib/CoreLib.java
@@ -4,6 +4,6 @@ public class CoreLib {
     public static final String MOD_ID           = "SlimevoidLib";
     public static final String MOD_NAME         = "Slimevoid Library";
     public static final String MOD_VERSION      = "2.0.4.5";
-    public static final String MOD_CLIENT_PROXY = "com.slimevoid.library.proxy.SV_ClientProxy";
-    public static final String MOD_COMMON_PROXY = "com.slimevoid.library.proxy.SV_CommonProxy";
+    public static final String MOD_CLIENT_PROXY = "net.slimevoid.library.proxy.SV_ClientProxy";
+    public static final String MOD_COMMON_PROXY = "net.slimevoid.library.proxy.SV_CommonProxy";
 }
