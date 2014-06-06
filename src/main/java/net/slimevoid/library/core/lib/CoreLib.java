7aa751659f7adb2a68fc21b6672fb68e4a8bc745
Eurymachus <reflex_ion@hotmail.com>
2014-06-06 14:26:50 +0100

    Build version update

--- src/main/java/net/slimevoid/library/core/lib/CoreLib.java
+++ src/main/java/net/slimevoid/library/core/lib/CoreLib.java
@@ -1,9 +1,11 @@
 package net.slimevoid.library.core.lib;
 
 public class CoreLib {
-    public static final String MOD_ID           = "SlimevoidLib";
-    public static final String MOD_NAME         = "Slimevoid Library";
-    public static final String MOD_VERSION      = "@VERSION@";
-    public static final String MOD_CLIENT_PROXY = "net.slimevoid.library.proxy.SV_ClientProxy";
-    public static final String MOD_COMMON_PROXY = "net.slimevoid.library.proxy.SV_CommonProxy";
+    public static final String MOD_ID              = "SlimevoidLib";
+    public static final String MOD_NAME            = "Slimevoid Library";
+    public static final String MOD_VERSION         = "@VERSION@";
+    public static final String MOD_DEPENDENCIES    = "";
+    public static final String ACTUAL_DEPENDENCIES = "required-after:Forge@[@FORGEVERSION@,)";
+    public static final String MOD_CLIENT_PROXY    = "net.slimevoid.library.proxy.SV_ClientProxy";
+    public static final String MOD_COMMON_PROXY    = "net.slimevoid.library.proxy.SV_CommonProxy";
 }
